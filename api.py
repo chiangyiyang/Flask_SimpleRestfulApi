ef33ce1e30c495227010237ea3894008cdcc08a0
chiangyiyang <chiangyiyang@gmail.com>
2019-09-22 11:46:31 +0800

    A Minimal API

--- api.py
+++ api.py
@@ -1,1 +1,14 @@
-print("Hello")
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
