b7c17885687b572ad864f7ba07593255dee52af4
chiangyiyang <chiangyiyang@gmail.com>
2019-09-22 13:46:03 +0800

    增加Device類別及其GET方法，已取得單一設備資料

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,13 +13,26 @@ parser.add_argument('id')
 parser.add_argument('lat')
 parser.add_argument('lng')
 
+
+def abort_if_device_doesnt_exist(device_id):
+    if device_id not in Devices:
+        abort(404, message="Device {} doesn't exist".format(device_id))
+
+# Device
+# shows a single Device item and lets you delete a Device item
+class Device(Resource):
+    def get(self, device_id):
+        #Testing Cmd: curl http://%IP%:%Port%/devices/a03
+        abort_if_device_doesnt_exist(device_id)
+        return Devices[device_id]
+
 class DeviceList(Resource):
     def get(self):      
-        #Testing Cmd: curl http://IP:Port/devices 
+        #Testing Cmd: curl http://%IP%:%Port%/devices 
         return Devices    
 
     def post(self):     
-        #Testing Cmd: curl -X POST -d "id=a04" -d "lat=23.5" -d "lng=121.5"  http://IP:Port/devices  
+        #Testing Cmd: curl -X POST -d "id=a04" -d "lat=23.5" -d "lng=121.5"  http://%IP%:%Port%/devices  
         args = parser.parse_args()
         if  args['id'] in Devices.keys():
             return "ID id duplicated!", 406
@@ -27,6 +40,7 @@ class DeviceList(Resource):
         return Devices[args['id']], 201
 
 api.add_resource(DeviceList, '/devices')
+api.add_resource(Device, '/devices/<device_id>')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)  # 將IP及Port設定成對外服務
