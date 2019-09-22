f617099cbbe71402d4776ebd8925d9040ae728f2
chiangyiyang <chiangyiyang@gmail.com>
2019-09-22 12:58:19 +0800

    重新命名Class Devices => DeviceList

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Devices(Resource):
+class DeviceList(Resource):
     def get(self):
         # 回傳3組測試資料
         return [                    
@@ -12,7 +12,7 @@ class Devices(Resource):
             {'id':'aa02', 'lat':23.52, 'lng':121.32},
             {'id':'aa03', 'lat':23.53, 'lng':121.33}]
 
-api.add_resource(Devices, '/devices')
+api.add_resource(DeviceList, '/devices')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)  # 將IP及Port設定成對外服務
